13eb67753283cf01101e500876554724b3d8b983
Noorish100 <mdnoorishnauman@gmail.com>
2022-04-18 13:16:25 +0530

    Update Child.java

--- eval2/ques4/Child.java
+++ eval2/ques4/Child.java
@@ -2,9 +2,22 @@ package com.masai;
 
 import java.util.Scanner;
 public final class Child extends Java{
+	Scanner sc=new Scanner(System.in);
 	@Override
 	void method1(){
-		System.out.println("child m1");
+		
+			System.out.println("write no");
+		int no=	sc.nextInt();
+			Java j2=new Child();
+		
+		if(no>0&&no<10){
+
+       System.out.println(sc.nextInt());
+
+		}
+		else{
+				System.out.println("invalid no");
+		}
 	}
 	void method4() {
 		System.out.println("m4 child");
